edf1db5783d857f0aabded09e7df84ba7ba09525
joslingg <truonghoanglinh787@gmail.com>
2025-07-03 15:35:57 +0700

    models-admin

--- medit_manager/procure/models.py
+++ medit_manager/procure/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class ProcurementRecord(models.Model):
-    code = models.CharField(max_length=20, unique=True, blank=True, verbose="Mã hồ sơ")
+    code = models.CharField(max_length=20, unique=True, blank=True, verbose_name="Mã hồ sơ")
     title = models.CharField(max_length=255, verbose_name="Tiêu đề")
     desciption = models.TextField(blank=True, null=True, verbose_name="Mô tả")
     date_created = models.DateField(auto_now_add=True, verbose_name="Ngày tạo")
@@ -14,6 +14,11 @@ class ProcurementRecord(models.Model):
             date_part = today.strftime("%d%m%y")
             self.code = f"{date_part}/{count_today}"
         super().save(*args,**kargs)
+        
+    class Meta:
+        ordering = ['code']
+        verbose_name = 'Hồ sơ'
+        verbose_name_plural = 'Hồ sơ'
         
     def __str__(self):
         return self.code or self.title
